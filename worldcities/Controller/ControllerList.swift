50bb679ef48825dc0ac54b995ecf3d04430662b9
vauxhall <zero.griffin@gmail.com>
2017-08-10 12:02:11 -0500

    Create ControllerList

---
+++ worldcities/Controller/ControllerList.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class ControllerList:UIViewController
+{
+    init()
+    {
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
