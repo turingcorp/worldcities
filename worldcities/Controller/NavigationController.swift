cb0b8f3b0f478084a3db9aff675972fb3200aa0e
vauxhall <zero.griffin@gmail.com>
2017-08-10 12:14:56 -0500

    Add list to navigation

--- worldcities/Controller/NavigationController.swift
+++ worldcities/Controller/NavigationController.swift
@@ -5,6 +5,9 @@ class NavigationController:UINavigationController
     init()
     {
         super.init(nibName:nil, bundle:nil)
+        
+        let list:ControllerList = ControllerList()
+        addChildViewController(list)
     }
     
     required init?(coder:NSCoder)
