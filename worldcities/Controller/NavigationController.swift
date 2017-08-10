a348987daf082856abc6209ff9e805b2610d84d6
vauxhall <zero.griffin@gmail.com>
2017-08-10 12:11:26 -0500

    Create navigation controller

---
+++ worldcities/Controller/NavigationController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class NavigationController:UINavigationController
+{
+    init()
+    {
+        super.init(nibName:nil, bundle:nil)
+    }
+    
+    required init?(coder:NSCoder)
+    {
+        return nil
+    }
+}
