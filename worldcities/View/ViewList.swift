431e686759c635ccc0621ea1a3a96da292d359ae
vauxhall <zero.griffin@gmail.com>
2017-08-10 12:55:37 -0500

    Add collection view

--- worldcities/View/ViewList.swift
+++ worldcities/View/ViewList.swift
@@ -1,8 +1,14 @@
 import UIKit
 
-class ViewList:UIView
+class ViewList:
+    UIView,
+    UICollectionViewDelegate,
+    UICollectionViewDataSource,
+    UICollectionViewDelegateFlowLayout
 {
+    private(set) weak var collectionView:UICollectionView!
     private weak var controller:ControllerList!
+    private let kCellSeparation:CGFloat = 1
     
     init(controller:ControllerList)
     {
@@ -10,10 +16,44 @@ class ViewList:UIView
         clipsToBounds = true
         backgroundColor = UIColor.white
         self.controller = controller
+        
+        factoryCollectionView()
     }
     
     required init?(coder:NSCoder)
     {
         return nil
+    }
+    
+    //MARK: private
+    
+    private func factoryCollectionView()
+    {
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.scrollDirection = UICollectionViewScrollDirection.vertical
+        flow.headerReferenceSize = CGSize.zero
+        flow.footerReferenceSize = CGSize.zero
+        flow.minimumLineSpacing = kCellSeparation
+        flow.minimumInteritemSpacing = kCellSeparation
+        flow.sectionInset = UIEdgeInsets(
+            top:kCellSeparation,
+            left:0,
+            bottom:kCellSeparation,
+            right:0)
+        
+        let collectionView:UICollectionView = UICollectionView(
+            frame:CGRect.zero,
+            collectionViewLayout:flow)
+        collectionView.clipsToBounds = true
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.clear
+        collectionView.showsVerticalScrollIndicator = true
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.alwaysBounceVertical = true
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        self.collectionView = collectionView
+        
+        addSubview(collectionView)
     }
 }
