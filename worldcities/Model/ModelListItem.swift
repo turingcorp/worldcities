3a0e2383b05bd675ea4e6d189c71842d61f30991
vauxhall <zero.griffin@gmail.com>
2017-08-10 22:38:42 -0500

    Add compare string to item

--- worldcities/Model/ModelListItem.swift
+++ worldcities/Model/ModelListItem.swift
@@ -3,6 +3,7 @@ import Foundation
 class ModelListItem
 {
     let identifier:Int
+    let compareString:String
     let name:String
     let country:String
     let latitude:Double
@@ -20,5 +21,12 @@ class ModelListItem
         self.country = country
         self.latitude = latitude
         self.longitude = longitude
+        
+        let nameLowercase:String = name.lowercased()
+        let countryLowercase:String = country.lowercased()
+        
+        var compareString = nameLowercase
+        compareString.append(countryLowercase)
+        self.compareString = compareString
     }
 }
