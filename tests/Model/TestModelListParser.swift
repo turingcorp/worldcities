2baad00e0386b7b30b5ff65fd0846198c1e1be4b
vauxhall <zero.griffin@gmail.com>
2017-08-10 21:42:50 -0500

    Create test for load data

--- tests/Model/TestModelListParser.swift
+++ tests/Model/TestModelListParser.swift
@@ -3,9 +3,32 @@ import XCTest
 
 class TestModelListParser:XCTestCase
 {
+    var modelList:ModelList?
+    
+    override func setUp()
+    {
+        super.setUp()
+        modelList = ModelList()
+    }
+    
     func testFactoryResourceUrl()
     {
         let url:URL? = ModelList.factoryResourceUrl()
         XCTAssertNotNil(url, "failed creating resource url")
+    }
+    
+    func testLoadData()
+    {
+        guard
+            
+            let url:URL = ModelList.factoryResourceUrl()
+        
+        else
+        {
+            return
+        }
+        
+        let data:Data? = modelList?.loadData(url:url)
+        XCTAssertNotNil(data, "failed loading data from url")
     }
 }
