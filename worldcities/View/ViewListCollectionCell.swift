0b679214caf8185d4c16ee6e1d8ab60eb50da2d2
vauxhall <zero.griffin@gmail.com>
2017-08-11 01:27:23 -0500

    Add label to cell

--- worldcities/View/ViewListCollectionCell.swift
+++ worldcities/View/ViewListCollectionCell.swift
@@ -3,12 +3,57 @@ import UIKit
 class ViewListCollectionCell:UICollectionViewCell
 {
     static let reusableIdentifier:String = "listCell"
+    private weak var label:UILabel!
+    private let kFontSize:CGFloat = 16
+    private let kLabelMarginHorizontal:CGFloat = 15
     
     override init(frame:CGRect)
     {
         super.init(frame:CGRect.zero)
         clipsToBounds = true
         backgroundColor = UIColor.white
+        
+        let label:UILabel = UILabel()
+        label.isUserInteractionEnabled = false
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = UIColor.clear
+        label.font = UIFont.systemFont(ofSize:kFontSize)
+        self.label = label
+        
+        addSubview(label)
+        
+        NSLayoutConstraint(
+            item:label,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.top,
+            multiplier:1,
+            constant:0).isActive = true
+        NSLayoutConstraint(
+            item:label,
+            attribute:NSLayoutAttribute.bottom,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.bottom,
+            multiplier:1,
+            constant:0).isActive = true
+        NSLayoutConstraint(
+            item:label,
+            attribute:NSLayoutAttribute.left,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.left,
+            multiplier:1,
+            constant:kLabelMarginHorizontal).isActive = true
+        NSLayoutConstraint(
+            item:label,
+            attribute:NSLayoutAttribute.right,
+            relatedBy:NSLayoutRelation.equal,
+            toItem:self,
+            attribute:NSLayoutAttribute.right,
+            multiplier:1,
+            constant:-kLabelMarginHorizontal).isActive = true
     }
     
     required init?(coder:NSCoder)
