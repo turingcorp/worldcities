5c3f948e5b6c788b9a246a610cbc76320a53522e
vauxhall <zero.griffin@gmail.com>
2017-08-11 01:13:46 -0500

    Add clean input string

--- worldcities/Model/ModelListSearch.swift
+++ worldcities/Model/ModelListSearch.swift
@@ -4,6 +4,17 @@ extension ModelList
 {
     func searchItems(forInput:String) -> [ModelListItem]
     {
+        let cleanedInput:String = cleanInput(input:forInput)
+        
         return []
+    }
+    
+    //MARK: private
+    
+    private func cleanInput(input:String) -> String
+    {
+        let cleaned:String = input
+        
+        return cleaned
     }
 }
