8d1e98f9d72439a29e0f27718277a01296542a6e
vauxhall <zero.griffin@gmail.com>
2017-08-10 21:47:36 -0500

    Create test for load json

--- tests/Model/TestModelListParser.swift
+++ tests/Model/TestModelListParser.swift
@@ -31,4 +31,20 @@ class TestModelListParser:XCTestCase
         let data:Data? = modelList?.loadData(url:url)
         XCTAssertNotNil(data, "failed loading data from url")
     }
+    
+    func testLoadJson()
+    {
+        guard
+            
+            let url:URL = ModelList.factoryResourceUrl(),
+            let data:Data = modelList?.loadData(url:url)
+            
+        else
+        {
+            return
+        }
+        
+        let json:Any? = modelList?.loadJson(data:data)
+        XCTAssertNotNil(json, "failed loading json from data")
+    }
 }
