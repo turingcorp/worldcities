af3f4cffd23f12b6b0fdb8101226abf15f7609bd
vauxhall <zero.griffin@gmail.com>
2017-08-10 21:12:05 -0500

    Create test for factory url

--- tests/Model/TestModelListParser.swift
+++ tests/Model/TestModelListParser.swift
@@ -1,5 +1,11 @@
 import XCTest
+@testable import worldcities
 
 class TestModelListParser:XCTestCase
 {
+    func testFactoryResourceUrl()
+    {
+        let url:URL? = ModelList.factoryResourceUrl()
+        XCTAssertNotNil(url, "failed creating resource url")
+    }
 }
