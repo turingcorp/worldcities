db4aeda4cab1c70f045cf97b04d8366c1ced0539
vauxhall <zero.griffin@gmail.com>
2017-08-11 01:09:27 -0500

    Add test for search with tab

--- tests/Model/TestModelListSearch.swift
+++ tests/Model/TestModelListSearch.swift
@@ -5,6 +5,8 @@ class TestModelListSearch:XCTestCase
 {
     private let kStringEmpty:String = ""
     private let kStringWithSpaces:String = "   "
+    private let kStringWithNewLine:String = "\n"
+    private let kStringWithTab:String = "\t"
     private let kWaitExpectation:TimeInterval = 90
     
     func testSearchItems()
@@ -22,6 +24,8 @@ class TestModelListSearch:XCTestCase
         { [weak self] (error:Error?) in
             
             self?.inputEmpty(modelList:modelList)
+            self?.inputSpaces(modelList:modelList)
+            self?.inputTab(modelList:modelList)
         }
     }
     
@@ -51,5 +55,18 @@ class TestModelListSearch:XCTestCase
             foundCount,
             totalCount,
             "failed searching with spaces on input")
+    }
+    
+    private func inputTab(modelList:ModelList)
+    {
+        let foundItems:[ModelListItem] = modelList.searchItems(
+            forInput:kStringWithTab)
+        let foundCount:Int = foundItems.count
+        let totalCount:Int = modelList.items.count
+        
+        XCTAssertEqual(
+            foundCount,
+            totalCount,
+            "failed searching with tab on input")
     }
 }
